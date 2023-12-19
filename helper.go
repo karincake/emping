b5509f777653e4763a15b733343c1649c0655467
Munawwirul Jamal <munawwirul.jamal@gmail.com>
2023-12-19 14:04:13 +0700

    feat/basic-test: init commit

---
+++ helper.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+)
+
+func formatJSON(data []byte) string {
+	var out bytes.Buffer
+	err := json.Indent(&out, data, "", " ")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	d := out.Bytes()
+	return string(d)
+}
---
+++ test/readme.md
@@ -0,0 +1,2 @@
+# emping - test
+We use https://github.com/munaja/blog-practice-be-using-go for testing. Please create a new database and do the database migration from that repo for testing.
